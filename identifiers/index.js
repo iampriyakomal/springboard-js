f4e23a05851db2cd4697ddd577067efb80db2f9f
iampriyakomal <167619037+iampriyakomal@users.noreply.github.com>
2025-02-01 02:04:36 +0530

    identifiers by let, const and var

---
+++ identifiers/index.js
@@ -0,0 +1,12 @@
+var tempFahrenheit = 99;
+let TempFahrenheit = 99;
+// let TempFahrenheit=99; Redeclaration is not allowed 
+
+const TEMP_CELSIUS = 38;
+
+// document.write() is used to write content onto the HTML page
+document.write("Default temperature (var) is: " + tempFahrenheit + "</span>");
+document.write("<br/>");
+document.write("Default temperature (let) is: " + TempFahrenheit + "</span>");
+document.write("<br/>");
+document.write("Normal body temperature in Celsius is: " + TEMP_CELSIUS + "</span>");
